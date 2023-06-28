e96103473880a5016d546d2eb61d9906f7bfb8c2
lmpko <lmpkoo@gmail.com>
2023-06-28 11:56:06 +0200

    form complete component created with html structure

---
+++ src/ThankComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ThankComponent = () => {
+  return (
+    <div>
+        <img src="" alt=""/>
+        <h1>Thank you!</h1>
+        <p>
+            We've added your card details
+        </p>
+        <button type="">Continue</button>
+    </div>
+  )
+}
+
+export default ThankComponent
