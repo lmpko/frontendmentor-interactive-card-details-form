b898f740703ca19302f8d1d72ba83cef886bce6c
lmpko <lmpkoo@gmail.com>
2023-06-28 11:32:04 +0200

    app.js file created with react added, layout component and form component created

---
+++ src/LayoutComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import FormComponent from './FormComponent'
+
+const LayoutComponent = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+        <div>
+            <FormComponent/>
+        </div>
+    </div>
+  )
+}
+
+export default LayoutComponent
