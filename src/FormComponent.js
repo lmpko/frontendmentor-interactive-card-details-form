2598b0da068818a88853182fa663439c3b54a828
lmpko <lmpkoo@gmail.com>
2023-06-28 11:53:30 +0200

    formcomponent structure added

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const FormComponent = () => {
   return (
-    <div>FormComponent</div>
+    <form>
+      <label for="">
+        Cardholder Name
+        <input type="" name="" value="" placeholder='e.g. Jane Appleseed'/>
+      </label>
+      
+      <label for="">
+        Card Number
+        <input type="" name="" value="" placeholder='e.g. 1234 5678 9123 0000'/>
+      </label>
+
+      <label for="">
+        Exp. Date (MM/YY)
+        <input type="date" name="" value="" placeholder=''/>
+        <input type="date" name="" value="" placeholder=''/>
+      </label>
+      
+      <label for="">
+        CVC
+        <input type="" name="" value="" placeholder='e.g. 123'/>
+      </label>
+      
+      
+      <button type="">Confirm</button>
+    </form>
   )
 }
 
